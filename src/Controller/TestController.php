ef3dae47ed45d864bf1f049cb5ec52cd991bf5cd
Franck <patouillardfranck3@gmail.com>
2022-06-17 17:00:06 +0200

    new controller structure

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,8 +11,7 @@ class TestController extends AbstractController
     /**
      * @Route("/login", name="app_test")
      */
-    public function index(Request $request): JsonResponse
-    {
+    public function index(Request $request): JsonResponse {
         if($request->getMethod() === 'POST' && $request->headers->get('username') == 'patouillardfranck3@gmail.com' && $request->get('password') == 'Franck97'){
             return new JsonResponse([
                 'api_key' => 'AGAEGEARGEAFEAEFAFEAFAZEFAE'
@@ -26,7 +25,7 @@ class TestController extends AbstractController
     /**
      * @Route("/users/lists", name="app_users")
      */
-    public function getUsers(Request $request){
+    public function getUsers(Request $request): JsonResponse {
         if($request->getMethod() === 'GET' && $request->headers->get('Authorization') === 'AGAEGEARGEAFEAEFAFEAFAZEFAE' && $request->get('username') == 'patouillardfranck3@gmail.com' && $request->get('password') == 'Franck97'){
             return new JsonResponse(["Patouillard", "Franck", 24, "Développeur web alternance"]);
         } else {
